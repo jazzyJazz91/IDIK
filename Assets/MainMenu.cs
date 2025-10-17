c12893a2ef8abe5c9f67ade9696409048d0b1d32
jazzyJazz91 <131871779+jazzyJazz91@users.noreply.github.com>
2025-10-17 12:52:08 +0200

    start page working

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
